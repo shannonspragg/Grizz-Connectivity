17b02057e76019b45c32ff3159cfb04f254c38cb
Shannon Spragg <shannonspragg@u.boisestate.edu>
2022-04-26 11:34:09 -0600

    saving resistance input

--- analysis code scripts/Prob Conflict Resistance Surface.R
+++ analysis code scripts/Prob Conflict Resistance Surface.R
@@ -13,7 +13,6 @@ library(ggmap)
 library(rgdal)
 library(maptools)
 library(viridis)
-library(googledrive)
 
 
 # Create our Rescale Function: --------------------------------------------
@@ -24,28 +23,26 @@ rescale01 <- function(r1) {
 
 # Bring in Rasters: -----------------------------------------------
 
-r <- raster("Data/original/SOI_10km.tif") 
+r <- raster("Data/processed/ona_bound.tif") 
 p.bear.conf <- raster("Data/original/prob_bear_conflict.tif")
 grizz.dens <- raster("Data/original/grizz_dens.tif")
 
+
 # NOTE: need to check p(conflict values so highest resistance is highest p(conflict))
 
-# Crop and Resample Source Raster: ----------------------------------------
-
-grizzdens.crop <- raster::crop(grizz.dens, r)  
-
-grizzdens.rsmple <- resample(grizzdens.crop, r, method='bilinear')
-
-grizzdens.soi <- terra::mask(grizzdens.rsmple, r) 
-
+  # Resample to match the extent and resolution of template raster
+p.conf.resample <- resample(p.bear.conf, grizz.dens)
+plot(p.conf.resample)
+plot(grizz.dens, add=TRUE)
 
 # Rescale our Raster for Standardization: ---------------------------------
   # Rescale to 0-1 for standardization
   # set the NA values to 1 for highest resistance (won't run in CS otherwise)
-p.conf.rescale <- rescale01(p.bear.conf)
+p.conf.rescale <- rescale01(p.conf.resample)
 p.conf.rescale[is.na(p.conf.rescale)]=1
 p.conf.rescale
 plot(p.conf.rescale, col=plasma(256), axes = TRUE, main = "Probability of Bear Conflict Resistance Layer")
+
 
 
 # Make this a Resistance Surface: -----------------------------------------
@@ -56,5 +53,4 @@ plot(p_conflict_resistance, col=plasma(256), axes = TRUE, main = "Probability of
 # Save our Raster: --------------------------------------------------------
   # Write raster (saving both gdrive and local computer):
 writeRaster(p_conflict_resistance, "Data/processed/p_conflict_resistance_layer.tif", overwrite = TRUE)
-writeRaster(grizzdens.soi, "Data/processed/grizz_dens_soi.tif", overwrite = TRUE)
 
