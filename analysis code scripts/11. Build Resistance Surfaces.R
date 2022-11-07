11bd3933924899bc78090bd9244b5350c750861e
Shannon Spragg <shannonspragg@u.boisestate.edu>
2022-11-07 11:45:22 -0700

    updating these

--- analysis code scripts/11. Build Resistance Surfaces.R
+++ analysis code scripts/11. Build Resistance Surfaces.R
@@ -5,21 +5,25 @@
 library(terra)
 library(sf)
 library(tidyverse)
+library(raster)
 
 # Data:
 bhs <- rast("Data/processed/bhs_ONA.tif")
 ghm1 <- rast("Data/original/gHMv1_300m_2017_static/gHMv1_300m_2017_static-0000000000-0000000000.tif")
-ghm2 <- rast("/Users/mattwilliamson/Google Drive/My Drive/SpaSES Lab/Shared Data Sets/Wildlife Survey/data/original/rasters/gHMv1_300m_2017_static-0000046592-0000000000.tif")
-
-
-ghm1.crp <- project(ghm1, bhs)
-ghm2.crp <- project(ghm2, bhs)
-ghm.mos <- mosaic(ghm1.crp, ghm2.crp, fun="max")
-ghm.conv <- ghm.mos/65536
+ghm2 <- rast("Data/original/gHMv1_300m_2017_static/gHMv1_300m_2017_static-0000046592-0000000000.tif")
 
 ona.bound <- st_read("Data/original/ONA_TerritoryBound.shp") %>% 
   st_buffer(., 100000) %>% st_transform(., crs=crs(bhs)) %>% 
   as(., "SpatVector")
+bhs.ona <- crop(bhs, project(ona.bound, bhs))
+writeRaster(bhs.ona, "Data/processed/bhs_ONA_crop.tif")
+
+ghm1.crp <- project(ghm1, bhs.ona)
+ghm2.crp <- project(ghm2, bhs.ona)
+ghm.mos <- mosaic(ghm1.crp, ghm2.crp, fun="max")
+ghm.conv <- ghm.mos/65536
+
+#ghm.crop <- crop(ghm.conv, project(ona.bound, ghm.conv))
 elev.can <- rast(raster::getData('alt', country = 'CAN'))
 elev.us <- rast(raster::getData('alt', country = 'USA')[[1]])
 elev.can.crop <- crop(elev.can, project(ona.bound, elev.can))
@@ -30,7 +34,7 @@ rough <- terrain(elev.mos, v="TRI")
 rough.max <-  global(rough, "max", na.rm=TRUE)[1,]
 rough.min <-  global(rough, "min", na.rm=TRUE)[1,]
 rough.rescale <- (rough - rough.min)/(rough.max - rough.min)
-rough.proj <- project(rough.rescale, bhs)
+rough.proj <- project(rough.rescale, bhs.ona)
 
 fuzzysum2 <- function(r1, r2) {
   rc1.1m <- (1-r1)
@@ -39,9 +43,9 @@ fuzzysum2 <- function(r1, r2) {
 }
 # Add together our biophys attributes: gHM and roughness
 biophys_fuzsum <- fuzzysum2(ghm.conv, rough.proj)
-writeRaster(biophys_fuzsum,"Data/processed/biophys_fuzsum.tif" )
+writeRaster(biophys_fuzsum,"Data/processed/biophys_fuzsum_ona.tif" )
 biophys_resistance <- (1+biophys_fuzsum)^10
-writeRaster(biophys_resistance, "Data/processed/biophys_resist.tif")
+writeRaster(biophys_resistance, "Data/processed/biophys_resist_ona.tif")
 
 
 # Add prob conflict for biophys + social surface --------------------------
